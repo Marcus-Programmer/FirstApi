7f4a70b44e05eaf0e59c03e3b79c225d4e4d0393
marcus-mendonca <hiohio20@gmail.com>
2022-07-06 10:24:25 -0300

    Commit incompleto

---
+++ src/controllers/UserControler.js
@@ -0,0 +1,33 @@
+const users = require("../mocks/users");
+
+module.exports = {
+  listUsers(request, response) {
+    const { order } = request.query;
+    const sortedUsers = users.sort((a, b) => {
+      if (order === "desc") {
+        return a.id < b.id ? 1 : -1;
+      }
+      return a.id > b.id ? 1 : -1;
+    });
+    response.send(200, sortedUsers);
+  },
+  getUserById(request, response) {
+    const { id } = request.params;
+    const user = users.find((user) => user.id === Number(id));
+    if (!user) {
+      return response.send(400, { error: "user not found" });
+    }
+    response.send(200, user);
+  },
+  createUser(request, response) {
+    const { body } = request;
+
+    const lastUserID = users[users.length - 1].id;
+    const newUser = {
+      id: lastUserID + 1,
+      name: body.name,
+    };
+    users.push(newUser);
+    response.send(200, newUser);
+  },
+};
---
+++ src/mocks/users.js
@@ -0,0 +1,17 @@
+module.exports = [
+
+{
+
+    id:1,
+    name:'Marcus'
+},{
+
+    id:2,
+    name:'Amanda'
+},{
+
+    id:3,
+    name:'Welisinho'
+},
+
+];
